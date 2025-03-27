1d6d169e0e2f243c9834997a0942addac4638aff
[Emama Shahzad] <[umamashahzad2004@gmail.com]>
2025-03-27 19:53:49 +0500

    Firebase Initialization

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -14,3 +14,23 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyArXwksugU7NwrW8D_Pt63FftzG_gKrDnA",
+  authDomain: "finsage-8fade.firebaseapp.com",
+  projectId: "finsage-8fade",
+  storageBucket: "finsage-8fade.firebasestorage.app",
+  messagingSenderId: "1015585315673",
+  appId: "1:1015585315673:web:b6f9126ed53046c8374a48",
+  measurementId: "G-J8FE6BTHF9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
