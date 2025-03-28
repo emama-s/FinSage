6d725a95dce6620a5452291878dc9d082c60e54d
[Emama Shahzad] <[umamashahzad2004@gmail.com]>
2025-03-28 14:10:52 +0500

    Added Auth

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,32 +1,15 @@
-/**
- * @license
- * Copyright 2022 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
- import { useState, useEffect } from 'react';
- import Head from 'next/head';
- import { useRouter } from 'next/router';
- import { Alert, Button, CircularProgress, Container, Dialog, DialogContent, DialogActions, Divider, IconButton, Snackbar, Stack, Typography } from '@mui/material';
- import AddIcon from '@mui/icons-material/Add';
- import NavBar from '../components/navbar';
- import ReceiptRow from '../components/receiptRow';
- import ExpenseDialog from '../components/expenseDialog';
- import { useAuth } from '../firebase/auth';
- import { deleteReceipt, getReceipts } from '../firebase/firestore';
- import { deleteImage } from '../firebase/storage';
- import styles from '../styles/dashboard.module.scss';
+import { useState, useEffect } from 'react';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { Alert, Button, CircularProgress, Container, Dialog, DialogContent, DialogActions, Divider, IconButton, Snackbar, Stack, Typography } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import NavBar from '../components/navbar';
+import ReceiptRow from '../components/receiptRow';
+import ExpenseDialog from '../components/expenseDialog';
+import { useAuth } from '../firebase/auth';
+import { deleteReceipt, getReceipts } from '../firebase/firestore';
+import { deleteImage } from '../firebase/storage';
+import styles from '../styles/dashboard.module.scss';
 
 const ADD_SUCCESS = "Receipt was successfully added!";
 const ADD_ERROR = "Receipt was not successfully added!";
